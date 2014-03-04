778abfb5ecdddfb723650861210a914c6a78275c
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-03-04 22:22:24 +0530

    Python: Made some changes in classexmp.py for demonstrating some built-in class attributes.

--- py/classexmp.py
+++ py/classexmp.py
@@ -45,6 +45,12 @@ def main():
 	
 	displayCount()
 	
+	print "Employee.__doc__:", Employee.__doc__
+	print "Employee.__name__:", Employee.__name__
+	print "Employee.__module__:", Employee.__module__
+	print "Employee.__bases__:", Employee.__bases__
+	print "Employee.__dict__:", Employee.__dict__
+	
 	return 0
 
 if __name__ == '__main__':
