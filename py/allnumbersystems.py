25bbee75e3c2660be0ebb3c94062368de592c4e1
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-03-16 22:13:59 +0530

    py: Showing format for all number systems.

---
+++ py/allnumbersystems.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Screen Off and Lock
+#  
+#  Copyright 2014 Ajay Bhatia <ajay@dumb-box>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+import sys
+
+def main(number):
+	number = int(number)
+	# Without 0b, 0o, or 0x as prefix:
+	print "bin: {0:b}, oct: {0:o}, dec: {0:d}, hex: {0:x}".format(number)
+	return 0
+
+if __name__ == '__main__':
+	try:
+		main(sys.argv[1])
+	except:
+		print "Usage: allnumbersystems.py <number>"
